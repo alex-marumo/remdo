5fadc311bca7b200e1750c345e686aaf50c47c54
Piotr Lewalski <piotr@lewalski.pl>
2022-10-17 15:03:15 +0000

    working on note menu

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,18 +1,20 @@
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { CollaborationPlugin } from '@lexical/react/LexicalCollaborationPlugin';
-import { $createParagraphNode, $createTextNode, $getRoot, $getSelection } from "lexical";
 import { TreeView } from "@lexical/react/LexicalTreeView";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useEffect } from "react";
-import { TextNode } from "lexical";
+import { NotesPlugin, NoteNode } from "./Notes";
+
+import "./Editor.css"
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ListNode, ListItemNode } from '@lexical/list';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
+
+/*
+import { CollaborationPlugin } from '@lexical/react/LexicalCollaborationPlugin';
 import { WebsocketProvider } from 'y-websocket';
 import { Doc } from 'yjs';
-import { NotesPlugin, EmojiNode, EmoticonPlugin, NoteNode } from "./Notes";
-import "./Editor.css"
 
 function createWebsocketProvider(id, yjsDocMap,) {
     let doc = yjsDocMap.get(id);
@@ -25,23 +27,7 @@ function createWebsocketProvider(id, yjsDocMap,) {
     }
     return new WebsocketProvider('ws://athena:8080', 'notes/0/' + id, doc, { connect: false, },);
 }
-
-function loadContent() {
-    const root = $getRoot();
-    root.clear();
-    const p = $createParagraphNode();
-    p.append($createTextNode('Loaded node 1'));
-    p.append($createTextNode('Loaded node 3'));
-    root.append(p);
-}
-
-function onChange(editorState) {
-    editorState.read(() => {
-        //const root = $getRoot();
-        //const selection = $getSelection();
-        //console.log(root, selection);
-    });
-}
+*/
 
 function Placeholder() {
     return <div className="editor-placeholder">Enter some plain text...</div>;
@@ -66,27 +52,40 @@ export default function Editor() {
         onError(error) {
             throw error;
         },
-        nodes: [EmojiNode, NoteNode],
+        nodes: [NoteNode, ListNode, ListItemNode],
+        theme: {
+            list: {
+                nested: {
+                  listitem: 'position-relative',
+                },
+                ol: 'editor-list-ol',
+                //ul: 'list-unstyled',
+                listitem: 'position-relative',
+              },
+        },
         //editorState: loadContent,
     };
     return (
         <div className="container">
+            <br />
             <LexicalComposer initialConfig={editorConfig}>
                 <div className="editor-container">
-                    <PlainTextPlugin
-                        contentEditable={<ContentEditable className="editor-input" />}
+                    <RichTextPlugin
+                        contentEditable={<div className="editor">
+                            <ContentEditable className="editor-input" />
+                        </div>}
                         placeholder={<Placeholder />}
                     />
-                    <OnChangePlugin onChange={onChange} />
+                    <ListPlugin />
+                    <AutoFocusPlugin />
                     <HistoryPlugin />
                     <TreeViewPlugin />
-                    <EmoticonPlugin />
                     <NotesPlugin />
-                    <CollaborationPlugin
+                    {/*<CollaborationPlugin
                         id="main"
                         providerFactory={createWebsocketProvider}
                         shouldBootstrap={true}
-                    />
+                    />*/}
                 </div>
 
             </LexicalComposer>
--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,114 +1,11 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { $insertBlockNode } from '@lexical/utils';
-import { $createLineBreakNode, $createParagraphNode, $createTextNode, $getRoot, $getSelection, COMMAND_PRIORITY_CRITICAL, COMMAND_PRIORITY_EDITOR, DecoratorNode, KEY_ENTER_COMMAND, KEY_TAB_COMMAND, TextNode } from 'lexical';
-import React, { useEffect } from 'react';
+import { $createTextNode, $getRoot, DecoratorNode, $getNodeByKey } from 'lexical';
+import React, { useEffect, useRef } from 'react';
 import "./Notes.css"
-
-export class EmojiNode extends TextNode {
-  static getType() {
-    return "emoji";
-  }
-
-  static clone(node) {
-    return new EmojiNode(node.__className, node.__text, node.__key);
-  }
-
-  constructor(className, text, key) {
-    super(text, key);
-    this.__className = className;
-  }
-
-  createDOM(config) {
-    const dom = document.createElement("span");
-    const inner = super.createDOM(config);
-    dom.className = this.__className;
-    inner.className = "emoji-inner";
-    dom.appendChild(inner);
-    return dom;
-  }
-
-  updateDOM(prevNode, dom, config) {
-    const inner = dom.firstChild;
-    if (inner === null) {
-      return true;
-    }
-    super.updateDOM(prevNode, inner, config);
-    return false;
-  }
-}
-
-export function $isEmojiNode(node) {
-  return node instanceof EmojiNode;
-}
-
-export function $createEmojiNode(className, emojiText) {
-  return new EmojiNode(className, emojiText).setMode("token");
-}
-
-function emoticonTransform(node) {
-  let pattern = ":)";
-  const textContent = node.getTextContent().slice(-pattern.length);
-  // When you type :), we will replace it with an emoji node
-  //console.log("ok", textContent)
-  if (textContent === pattern) {
-    let p = $createParagraphNode();
-    p.append($createTextNode(node.getTextContent().slice(0, -pattern.length)));
-    p.append($createEmojiNode("emoji happysmile", "🙂"));
-    node.replace(p);
-  }
-}
-
-function useEmoticons(editor) {
-  useEffect(() => {
-    const removeTransform = editor.registerNodeTransform(
-      TextNode,
-      emoticonTransform
-    );
-    return () => {
-      removeTransform();
-    };
-  }, [editor]);
-}
-
-
-export function EmoticonPlugin() {
-  const [editor] = useLexicalComposerContext();
-  useEmoticons(editor);
-  return null;
-}
-
-function Note(props) {
-  const [hover, setHover] = React.useState(false);
-  const [folded, setFolded] = React.useState(false);
-  const note = props.notes[props.id];
-
-  return (<li className='note'>
-    {
-      note.children
-      &&
-      <button type="button" className="btn btn-link shadow-none fold position-absolute start-0" onClick={() => setFolded(!folded)}>
-        {folded ? "+" : "-"}
-      </button>
-    }
-    <a href='/'>
-      <i
-        className={"bi align-middle p-1 " + (hover ? "bi-circle-fill" : "bi-circle")}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      ></i>
-    </a>
-    {note.text}
-    {
-      !folded
-      &&
-      <ul className='list-unstyled'>
-        {note.children && note.children.map((id) => <Note key={id.toString()} notes={props.notes} id={id} />)}
-      </ul>
-    }
-  </li>)
-}
+import { $createListNode, $createListItemNode, ListItemNode } from '@lexical/list';
 
 export class NoteNode extends DecoratorNode {
+
   static getType() {
     return "note";
   }
@@ -117,12 +14,8 @@ export class NoteNode extends DecoratorNode {
     return new NoteNode(node.__format, node.__key);
   }
 
-  //constructor(format, key) {
-  //  super(format, key);
-  //}
-
   createDOM(config) {
-    return document.createElement('div');
+    return document.createElement('span');
   }
 
   updateDOM(prevNode, dom, config) {
@@ -130,12 +23,16 @@ export class NoteNode extends DecoratorNode {
     if (inner === null) {
       return true;
     }
-    super.updateDOM(prevNode, inner, config);
     return false;
   }
 
   decorate() {
-    return <span>note6</span>;
+    return (
+    <span id="note-node" className='position-absolute top-0 start-0 note-menu'>
+      <a href="/">...</a>
+    </span>
+    )
+    //return <Note notes={{ 1: { text: "sample note" } }} id={1} />;
   }
 }
 
@@ -145,76 +42,47 @@ function $createNoteNode(className, emojiText) {
 
 export function NotesPlugin() {
   const [editor] = useLexicalComposerContext();
+  let activeNoteNode = useRef(null);
 
   useEffect(() => {
-    console.log("registering");
-    return;
-    return editor.registerCommand(
-      KEY_ENTER_COMMAND,
-      (event) => {
-        const focusNode = $getSelection().focus.getNode();
-        var br = $createLineBreakNode();
-        focusNode.insertAfter(br);
-        var node = $createNoteNode();
-        br.insertAfter(node);
-        //node.select();
-        event.preventDefault();
-        return true;
-      },
-      COMMAND_PRIORITY_CRITICAL,
-    );
+    return editor.registerMutationListener(ListItemNode, (mutatedNodes) => {
+      mutatedNodes.forEach((mutation, key) => {
+        if (mutation === 'created') {
+          let element = editor.getElementByKey(key);
+          element.addEventListener('mouseover', function (event) {
+            if (activeNoteNode.current != null) {
+              editor.update(() => {
+                $getNodeByKey(key).getFirstDescendant().insertAfter(activeNoteNode.current);
+              })
+            }
+          });
+        }
+      }
+      )
+    });
   }, [editor]);
 
-  function update() {
+  function clear() {
     editor.update(() => {
       const root = $getRoot()
       root.clear();
-      return;
-      console.log("root", root)
-      const p = $createParagraphNode();
-      p.append($createTextNode('Welcome to the playground'));
-      root.append(p);
+      const listNode = $createListNode();
+      for (let i = 0; i < 5; ++i) {
+        let listItemNode = $createListItemNode();
+        if (i === 2) {
+          const noteNode = $createNoteNode();
+          listItemNode.append(noteNode);
+          activeNoteNode.current = noteNode;
+        }
+        listItemNode.append($createTextNode("sample"));
+        listNode.append(listItemNode);
+      }
+      root.append(listNode);
     });
   }
-  return <button onClick={update}>Clear</button>;
-}
-
-
-export function Notes(props) {
-  const [notes] = React.useState({
-    1: {
-      children: [2, 3, 4, 5],
-      text: "sample note 1",
-    },
-    2: {
-      text: "sample note 2",
-      children: [6, 7]
-    },
-    3: {
-      text: "sample note 3",
-    },
-    4: {
-      text: "sample note 4",
-    },
-    5: {
-      text: "sample note 5",
-    },
-    6: {
-      text: "sample note 5",
-    },
-    7: {
-      text: "sample note 5",
-    },
-  });
-  let rootID = 1;
 
   return (
-    <div className="position-relative ps-2">
-      <ul className='list-unstyled'>
-        <Note notes={notes} id={rootID} />
-      </ul>
-    </div>
-  );
+    <div>
+      <button onClick={clear}>Clear</button>
+    </div>);
 }
-
-export default Notes;
