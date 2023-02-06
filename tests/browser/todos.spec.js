148364a055a790730ff8ea6c7e5f9366d5060dc1
Piotr Lewalski <piotr@lewalski.pl>
2023-02-06 15:22:53 +0000

    TODO test bugfix

--- tests/browser/todos.spec.js
+++ tests/browser/todos.spec.js
@@ -8,10 +8,13 @@ test("find todos", async ({ page }) => {
   --exclude-dir="node_modules" \
   --exclude-dir="lexical" \
   --exclude-dir="playwright-report" \
+  --exclude-dir=".cache" \
+  --exclude-dir="build" \
   --exclude="package-lock.json" \
   --exclude="todos.spec.js" \
   .`;
   const cmd = grepCmd + " || [ $? = 1 ]"; //do not fail if grep doesn't find anything and returns 1
+  console.log(grepCmd)
   const res = execSync(cmd).toString();
   if (res.length > 0) {
     console.warn("found some TODO or FIXME");
