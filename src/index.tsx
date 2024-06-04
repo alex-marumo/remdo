de5908c0f74f291ffeb624a2aae938df55535803
Piotr Lewalski <piotr@lewalski.pl>
2024-06-04 09:01:11 +0000

    adding console clearing trigger between hot reloads

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+//clear console on hot reload
+if (import.meta.hot) {
+  import.meta.hot.on("vite:beforeUpdate", () => console.clear());
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
