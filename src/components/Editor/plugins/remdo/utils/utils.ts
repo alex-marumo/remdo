8f3a054ed5e68ed90b4f2b16f8f699c387894d72
Piotr Lewalski <piotr@lewalski.pl>
2024-12-22 16:10:08 +0000

    adding printStack utility function

--- src/components/Editor/plugins/remdo/utils/utils.ts
+++ src/components/Editor/plugins/remdo/utils/utils.ts
@@ -16,3 +16,37 @@ globalThis.remdoGenerateNoteID = () => {
   return nanoid(8);
 };
 
+//TODO limit to dev
+globalThis.printStack = (message: string | undefined) => {
+  //TODO make the path easy to copy & paste
+  let res = message ? message + "\n" : "";
+  const stack = new Error().stack;
+  if (!stack) {
+    console.log("No stack available");
+    return;
+  }
+  stack.split("\n")
+    .slice(2) //skip "Error"" and this function
+    .forEach((line) => {
+      let functionName = "";
+      let path = "";
+      let row = "";
+      line.split(/\s+/)
+        .filter(word => word.trim() && word.trim() !== "at")
+        .forEach((word) => {
+          try {
+            const url = new URL(word.slice(1, -1));
+            const pathnameParts = url.pathname.split(":");
+            path = pathnameParts[0];
+            row = pathnameParts[1];
+          }
+          catch {
+            //not an URL, so it's likely a function name
+            functionName = word;
+          }
+        });
+      //query arg "a" is added, so chrome includes row in the link
+      res += `\t${functionName || '(anonymous)'} file://.${path}:${row}?a\n`;
+    });
+  console.log(res);
+};
