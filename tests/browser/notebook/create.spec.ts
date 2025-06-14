46f000921328fc637cbbd0cee3c5d5ee66df26ea
Piotr Lewalski <piotr@lewalski.pl>
2025-06-14 13:25:14 +0000

    adding split note test

--- tests/browser/notebook/create.spec.ts
+++ tests/browser/notebook/create.spec.ts
@@ -21,3 +21,13 @@ test("create some empty notes", async ({ page, notebook }) => {
   expect(await notebook.html()).toMatchSnapshot();
 });
 
+test("split note", async ({ page, notebook }) => {
+  await notebook.load("flat");
+  await notebook.clickEndOfNote("note1");
+
+  await page.keyboard.press("ArrowLeft");
+  await page.keyboard.press("ArrowLeft");
+  expect(await notebook.getNotes()).toEqual(['note0', 'note1', 'note2']);
+  await page.keyboard.press("Enter");
+  expect(await notebook.getNotes()).toEqual(['note0', 'not', 'e1', 'note2']);
+});
