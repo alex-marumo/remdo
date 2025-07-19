7ee251079b75b6272f504eff3ce0f53baba74391
alex_marumo <112119423+alex-marumo@users.noreply.github.com>
2025-07-19 10:27:49 +0200

    Update create.spec.ts

--- tests/browser/notebook/create.spec.ts
+++ tests/browser/notebook/create.spec.ts
@@ -20,7 +20,7 @@ test("add the first child to note with existing children", async ({ notebook, pa
   expect(await notebook.html()).toMatchSnapshot();
 });
 
-test('create some empty notes', async ({ page }) => {
+test('create some empty notes', async ({ notebook, page }) => {
   const notebook = new Notebook(page);
 
   // 1. Create two notes
