5e5bda2effa2ca5087114dd2e05f393a346f76c1
alex_marumo <112119423+alex-marumo@users.noreply.github.com>
2025-07-24 21:57:11 +0200

    Update notebook.spec.ts

--- tests/browser/notebook/notebook.spec.ts
+++ tests/browser/notebook/notebook.spec.ts
@@ -1,12 +1,57 @@
+import { htmlToCommaSeparatedText } from "tests/common";
 import { test } from "./common";
 import { expect } from "@playwright/test";
 
+test("move", async ({ page, notebook, menu }) => {
+  await notebook.load("flat");
+
+  let html = await notebook.html();
+  let notes = htmlToCommaSeparatedText(html);
+  expect(notes).toBe("note0,note1,note2");
+
+  await notebook.clickEndOfNote("note0");
+  await menu.open();
+
+  await page.keyboard.press("m");
+  await page.keyboard.press("ArrowDown");
+  await page.keyboard.press("ArrowDown");
+  await page.keyboard.press("Enter");
+
+  html = await notebook.html();
+  notes = htmlToCommaSeparatedText(html);
+  expect(notes).toBe("note1,note2,note0");
+});
+
+test.fixme("move and search", async ({ page, notebook, menu }) => {
+  await notebook.load("flat");
+
+  const initialNotes = await notebook.getNotes();
+  expect(initialNotes).toEqual(["note0", "note1", "note2"]);
+
+  await notebook.clickEndOfNote("note0");
+  await menu.open();
+  await page.keyboard.press("m");
+  await page.keyboard.type("note2");
+  await page.keyboard.press("Enter");
+
+  const updatedNotes = await notebook.getNotes();
+  expect(updatedNotes).toEqual(["note1", "note2", "note0"]);
+});
+
 test("load editor state", async ({ notebook }) => {
   await notebook.load("basic");
-  expect(await notebook.html()).toMatchSnapshot();
+
+  const html = await notebook.html();
+  expect(html).toContain("note0");
+  expect(html).toContain("note1");
+  expect(html).toContain("note2");
 });
 
 test("clear content", async ({ page, notebook }) => {
   await page.locator("text=Clear").click();
-  expect(await notebook.html()).toMatchSnapshot();
+
+  const html = await notebook.html();
+  expect(html).not.toContain("note0");
+  expect(html).not.toContain("note1");
+  expect(html).not.toContain("note2");
 });
