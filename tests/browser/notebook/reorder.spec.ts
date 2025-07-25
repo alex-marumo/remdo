7a32186a47cf7205ec385727d97785f6e85d48c0
alex_marumo <112119423+alex-marumo@users.noreply.github.com>
2025-07-25 14:44:34 +0200

    Update reorder.spec.ts

--- tests/browser/notebook/reorder.spec.ts
+++ tests/browser/notebook/reorder.spec.ts
@@ -58,7 +58,9 @@ test("reorder flat", async ({ page, notebook }) => {
   let order = htmlToCommaSeparatedText(await notebook.html()); 
   expect(order).toBe("note0,note1,note2");
 
-let html = await notebook.html(); let notes = htmlToCommaSeparatedText(html); expect(notes).toBe("note0,note1,note2");
+  let html = await notebook.html(); 
+  let notes = htmlToCommaSeparatedText(html);
+  expect(notes).toBe("note0,note1,note2");
 
   await page.keyboard.press("Meta+ArrowUp"); 
   order = htmlToCommaSeparatedText(await notebook.html()); 
